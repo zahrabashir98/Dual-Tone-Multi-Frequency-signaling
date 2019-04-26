7f291415febd17b2edc9414e7f5409b9a846cdae
zahrabashir98 <zahra_9877@yahoo.com>
2019-04-27 00:18:05 +0430

    Add fourie trasnform parts

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 """PyAudio example: Record a few seconds of audio and save to a WAVE file."""
+from scipy.fftpack import fft, ifft
+
+
+def fourie_transform(samples):
+    
+    x = np.array(samples)
+    y = fft(x)
+    print(y)
+    print(len(samples))
+    print(len(y))
+
+
+samples = []
+
+def convert_data(frames):
+    number_of_data = 0  
+ 
+
+    print("********************")
+    for data in frames:
+        audio_data = np.fromstring(data, np.int16)
+        print(audio_data)
+        for each in audio_data:
+            samples.append(each)
+
+    fourie_transform(samples)
+
 
 import pyaudio
 import wave
-
+import numpy as np
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-RECORD_SECONDS = 5
+RECORD_SECONDS = 0.1
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
@@ -27,6 +54,11 @@ for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     frames.append(data)
 
 print("* done recording")
+
+convert_data(frames)
+
+# dfft = 10.*np.log10(abs(np.fft.rfft(audio_data)))
+# print(dfft)
 
 stream.stop_stream()
 stream.close()
