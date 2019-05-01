f489ad0d344735accb2466e328d7682d47701ac6
zahrabashir98 <zahra_9877@yahoo.com>
2019-05-01 19:00:55 +0430

    Add main part of recognizing two highest frequences

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
 """PyAudio example: Record a few seconds of audio and save to a WAVE file."""
 from scipy.fftpack import fft, ifft
+import matplotlib.pyplot as plt
+import numpy as np
+import pyaudio
+import wave
+import numpy as np
+import math
 
+frequencies = [1209,1336,1477,1633,697,770,852,941]
+XJ = []
+sum = 0
+samples = []
+
+def calculate_N():
+    N = len(samples) / RECORD_SECONDS
+    print("N : %s"%N)
+    return N
 
 def fourie_transform(samples):
-    
+    N = calculate_N()
     x = np.array(samples)
-    y = fft(x)
-    print(y)
-    print(len(samples))
-    print(len(y))
+    for freq in frequencies:
+        sum = 0
+        for n in range(len(x)):
+            sum += x[n]*(np.exp(-1j* freq* N *n))
+        XJ.append(sum)
+    print(XJ)
+    print(XJ.index(max(XJ)))
+    print(XJ.index(XJ[len(XJ)-2]))
+    print("///////////////////////")
+
+    output = []
+    for data in XJ :
+        output.append(math.sqrt((data.real**2)+(data.imag**2)))
+    print(output)
+    print(output.index(max(output)))
+    print(output.index(output[len(output)-2]))
+    # print("///////////////////////")
+    # output = sorted(XJ, key=float)
+
+    # print(output[len(output)-1])
+    # print(max(output))
+    # print(output[len(output)-2])
 
 
-samples = []
+
+
+
 
 def convert_data(frames):
     number_of_data = 0  
@@ -20,21 +55,18 @@ def convert_data(frames):
     print("********************")
     for data in frames:
         audio_data = np.fromstring(data, np.int16)
-        print(audio_data)
+        # print(audio_data)
         for each in audio_data:
             samples.append(each)
-
+    # print(samples)
     fourie_transform(samples)
 
 
-import pyaudio
-import wave
-import numpy as np
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-RECORD_SECONDS = 0.1
+RECORD_SECONDS = 0.5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
